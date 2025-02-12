0d17ea7c54a5bdc962836de62acef0e8ca2faeaf
eeee-dot <ejamroz@visa.com>
2025-02-12 16:45:17 +0100

    main class

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,4 +1,23 @@
 package pl.coderslab;
 
+import pl.coderslab.daoClasses.UserDao;
+import pl.coderslab.entity.User;
+
 public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+//        user.setUsername("coder1");
+//        user.setPassword("coderslab1");
+//        user.setEmail("coderslab1@coderslab.pl");
+
+        UserDao userDao = new UserDao();
+//        userDao.create(user);
+//        System.out.println(user.getId());
+
+        User read = userDao.read(2);
+        System.out.println(read.getId());
+        System.out.println(read.getUsername());
+        System.out.println(read.getPassword());
+        System.out.println(read.getEmail());
+    }
 }
