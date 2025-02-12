ef726716b48aee1043a2c2d88774bcf23e0808c4
eeee-dot <ejamroz@visa.com>
2025-02-12 16:46:02 +0100

    Utility class for connection with database

--- src/main/java/pl/coderslab/DbUtils.java
+++ src/main/java/pl/coderslab/DbUtils.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtils {
-    private static String DB_URL =  "jdbc:mysql://localhost:3306/workshop2?useSSL=false&characterEncoding=utf8&serverTimezone=UTC";
+    private static String DB_URL =  "jdbc:mysql://localhost:3306/user_data?useSSL=false&characterEncoding=utf8&serverTimezone=UTC";
     private static String DB_USER = "root";
     private static String DB_PASS = "coderslab";
 
