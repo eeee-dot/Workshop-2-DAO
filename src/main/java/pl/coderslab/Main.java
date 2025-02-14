ed416d388534fab62212ceda7c0c14f541647fa4
eeee-dot <ejamroz@visa.com>
2025-02-14 16:27:26 +0100

    Tests for methods in UserDao.java class

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -3,21 +3,34 @@ package pl.coderslab;
 import pl.coderslab.daoClasses.UserDao;
 import pl.coderslab.entity.User;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        User user = new User();
-//        user.setUsername("coder1");
-//        user.setPassword("coderslab1");
-//        user.setEmail("coderslab1@coderslab.pl");
+//        User user = new User();
+//        user.setUsername("coder_update");
+//        user.setPassword("coderslab_update");
+//        user.setEmail("coderslab_updated@coderslab.pl");
 
         UserDao userDao = new UserDao();
 //        userDao.create(user);
 //        System.out.println(user.getId());
 
-        User read = userDao.read(2);
-        System.out.println(read.getId());
-        System.out.println(read.getUsername());
-        System.out.println(read.getPassword());
-        System.out.println(read.getEmail());
+//        User readUser = userDao.read(2);
+//        System.out.println(readUser.getId());
+//        System.out.println(readUser.getUsername());
+//        System.out.println(readUser.getPassword());
+//        System.out.println(readUser.getEmail());
+
+//        readUser.setUsername("coder_update");
+//        readUser.setPassword("coderslab_update");
+//        readUser.setEmail("coderslab_updated@coderslab.pl");
+//        userDao.update(readUser);
+
+//        userDao.delete(2);
+
+        User[] array = userDao.findAll("SELECT * FROM users WHERE email REGEXP '[a-z]+\\@[a-z]+\\.pl'");
+        System.out.println(Arrays.toString(array));
+
     }
 }
